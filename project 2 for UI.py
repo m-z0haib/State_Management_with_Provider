aa260c6d7ca113ce80dd2743df33aea8a1abeb8d
m-z0haib <zohaibmd97@gmail.com>
2024-06-26 02:10:45 +0500

    Update project 2 for UI.py

--- project 2 for UI.py
+++ project 2 for UI.py
@@ -1,26 +1,34 @@
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 
-
+# CREATED A DATA MODEL CLASS
 class ListItem:
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
-
+# CREATED A PROVIDER CLASS
 class ListProvider:
     def __init__(self):
         self.items = []
         self.id_counter = 1
-
+# ADD FUNCTION
     def add_item(self, name):
         new_item = ListItem(self.id_counter, name)
         self.items.append(new_item)
         self.id_counter += 1
-
+# REMOVE FUNCTION
     def remove_item(self, item_id):
         self.items = [item for item in self.items if item.id != item_id]
-
+# UPDATE FUNCTION
+    def remove_item(self, item_id):
+        self.items = [item for item in self.items if item.id != item_id]
+# UPDATE FUNCITON
+    def update_item(self, item_id, name):
+        for item in self.items:
+            if item.id == item_id:
+                item.name = new_name
+                break
 
 class App:
     def __init__(self, root):
